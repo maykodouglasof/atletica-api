19591ebdcf5ccf5d66d3ef1409431f1106e8e5b0
Mayko Douglas <maykodouglasbr@outlook.com>
2020-09-07 10:53:49 -0300

    Atualização

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cb4234ee862ec42d322fbd7dafe6afbb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b3500a3a-423e-4552-9c70-27be89209cdd'
+};
